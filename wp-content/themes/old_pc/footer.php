7fbc992471903bc972e4b61554e5d9b35d8cdf59
tobidot <Object.name@live.de>
2019-12-01 17:48:06 +0100

    generalize footer

--- wp-content/themes/old_pc/footer.php
+++ wp-content/themes/old_pc/footer.php
@@ -19,16 +19,12 @@
         <?php get_template_part('template-parts/footer/footer', 'widgets'); ?>
     </div>
     <div class="site-footer__site-info">
-        <div class="site-footer__sticker site-footer__name">
-            <div>
-                Project [S]elf
-            </div>
-        </div>
-        <a href="/impressum" class="site-footer__sticker site-footer__sticker--blue site-footer__impressum">
-            <div>
-                Impressum
-            </div>
-        </a>
+        <?php $items = wp_get_nav_menu_items('footer'); ?>
+        <?php foreach ($items as $menu_item) : /** @var WP_Post $menu_item*/ ?>
+            <a href="<?= $menu_item->url ?>" class="site-footer__sticker site-footer__sticker--blue site-footer__name">
+                <?= $menu_item->title ?>
+            </a>
+        <?php endforeach; ?>
     </div>
 </footer><!-- #colophon -->
 
