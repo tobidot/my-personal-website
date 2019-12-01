cc42865f6f28a2a22e891d47d33cfa7f004761f7
tobidot <Object.name@live.de>
2019-12-01 19:06:08 +0100

    Add content module

--- wp-content/themes/old_pc/index.php
+++ wp-content/themes/old_pc/index.php
@@ -5,7 +5,30 @@
 get_header();
 ?>
 
-Default Page
+<div id="primary" class="content-area">
+    <main id="main" class="site-main">
+
+        <?php
+        if (have_posts()) {
+
+            // Load posts loop.
+            while (have_posts()) {
+                the_post();
+                get_template_part('template-parts/content/content');
+            }
+
+            // Previous/next page navigation.
+            //twentynineteen_the_posts_navigation();
+        } else {
+
+            // If no content, include the "No posts found" template.
+            get_template_part('template-parts/content/content', 'none');
+        }
+        ?>
+
+    </main><!-- .site-main -->
+</div><!-- .content-area -->
+
 
 <?php
 get_footer();
