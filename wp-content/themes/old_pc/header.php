ede084f1733bf4f412998b941f756a47b3bd1301
tobidot <Object.name@live.de>
2019-12-01 18:55:29 +0100

    Add Main Menue working

--- wp-content/themes/old_pc/header.php
+++ wp-content/themes/old_pc/header.php
@@ -23,8 +23,23 @@
 <body <?php body_class(); ?>>
     <?php wp_body_open(); ?>
     <div id="page" class="site">
-        <header id="main-menue" class="">
-            <?= __('Main Menue', 'old_pc') ?>
+        <header id="main-menue" class="site-menu">
+            <?php
+            $locations = get_nav_menu_locations();
+            $menu = get_term($locations['menu-1'], 'nav_menu');
+            $menu_items = wp_get_nav_menu_items($menu->term_id);
+            ?>
+            <?php foreach ($menu_items as $menu_item) : /** @var WP_Post $menu_item*/ ?>
+                <div class="site-menu__entry">
+                    <div class="old-button">
+                        <div class="old-button__label">
+                            <?= $menu_item->title ?>
+                        </div>
+                        <a href="<?= $menu_item->url ?>" class="old-button__button">
+                        </a>
+                    </div>
+                </div>
+            <?php endforeach; ?>
         </header><!-- #header -->
 
         <div id="content" class="site-content monitor">
